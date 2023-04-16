3c63ecf230c27bff4b70ab75a8faa1357ba222b1
Pushpendra Kushwaha <89564501+pushpendraaa@users.noreply.github.com>
2023-04-16 13:22:37 +0530

    Update dash.py

--- Katoria/dash.py
+++ Katoria/dash.py
@@ -132,7 +132,7 @@ if selected == "Interactive Data Analysis":
         except Exception as e:
             print(e)
 if selected == "Insights":
-    image = Image.open('Katoria/crop.png')
+    image = Image.open('Katoria/crop.PNG')
     st.image(image, caption='Crop Calendar')
     with header_project:
         st.subheader("Crop Calendar")
